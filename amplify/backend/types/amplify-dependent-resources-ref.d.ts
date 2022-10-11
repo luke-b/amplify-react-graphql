7b6dab507529b85e02045c4e183634a48a5d0077
luke-b <luke.benda@gmail.com>
2022-10-11 16:06:39 +0200

    added storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s3c6b068b9": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
